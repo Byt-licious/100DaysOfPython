3a57e0afa9846e13fea8830e7902b7e2ccacb173
Beatrice Saganya <bitesaganya@gmail.com>
2023-12-20 23:34:13 +0300

    if else statement

---
+++ day-3-start/oddeven.py
@@ -0,0 +1,6 @@
+#write a program that works out whether a given number is odd or even.
+number = int(input("Enter any number: "))
+if(number % 2 == 0 ):
+    print("The number is even")
+else:
+    print("The number is odd")    
