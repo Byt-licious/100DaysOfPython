37397bdec7d85ef18a137216003091b2400a5a7a
Beatrice Saganya <bitesaganya@gmail.com>
2024-01-07 19:22:06 +0300

    nested lists

---
+++ day-4-start/lists.py
@@ -0,0 +1,5 @@
+fruits = ["Strawberries", "Nectaries", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kales", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
