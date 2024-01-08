de5cf8c505eaffab4b4cfe7f26405137cf08887a
Beatrice Saganya <bitesaganya@gmail.com>
2024-01-09 02:14:02 +0300

    fizzbuzz

--- day-5/rangetask.py
+++ day-5/rangetask.py
@@ -3,3 +3,11 @@ even_sum = 0
 for number in range(2, target + 1, 2):
     even_sum += number
 print(even_sum)    
+
+
+#alternative_sum = 0
+#for number in range(2, target + 1):
+     #if(number % 2) == 0:
+     #alternative_sum += number
+#print(alternative_sum)
+     
