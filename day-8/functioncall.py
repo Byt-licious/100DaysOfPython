aaa9f9b41f59ede0939749fd72a0fe075cecd984
Beatrice Saganya <bitesaganya@gmail.com>
2024-03-19 19:20:14 +0300

    function call with a name

--- day-8/functioncall.py
+++ day-8/functioncall.py
@@ -1,5 +1,12 @@
-def greet():
-    print("Hello")
-    print("How are you?")
-    print("How haqve you been")
-greet()    
+
+#def greet():
+    #print("Hello")
+    #print("How are you?")
+    #print("How haqve you been")
+#greet() 
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+    print(f"How have you been {name}")
+greet_with_name("Bytes") 
