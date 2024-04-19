cd005a362792417711d4c7aaf4042f2dee15e576
Beatrice Saganya <bitesaganya@gmail.com>
2024-04-19 21:43:51 +0300

    Decrypting text

--- day-8/encrypting.py
+++ day-8/encrypting.py
@@ -1,6 +1,6 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower
+text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 #1.Create afunction called encrypt that takes text and shift as input
 def encrypt(plain_text, shift_amount):
