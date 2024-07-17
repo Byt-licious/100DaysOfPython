a443c6ad6901ca49e47794ed9dfc0c777244b452
Beatrice Saganya <bitesaganya@gmail.com>
2024-07-17 11:29:39 +0300

    creating an empty dictionary

--- day-9/creatingdict.py
+++ day-9/creatingdict.py
@@ -1,7 +1,6 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
     "Function": "A piece of code that you can easily call over and over again.",
-    
 }
 
 #Retrieving an item from dictionary
@@ -10,3 +9,9 @@ print(programming_dictionary["Bug"])
 #Adding an item to the dictionary
 programming_dictionary["loop"] = "The action of doing something over and over again."
 print(programming_dictionary)
+
+#creating empty dictionary
+empty_dictionary = {}
+
+#programming_dictionary = {}
+#print(programming_dictionary)
