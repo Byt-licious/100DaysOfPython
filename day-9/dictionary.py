69cdac8539e56abab4dc4c5fc3e290df71e40210
Beatrice Saganya <bitesaganya@gmail.com>
2024-07-17 11:16:07 +0300

    Adding an item to the dictionary

--- day-9/dictionary.py
+++ day-9/dictionary.py
@@ -1,8 +1,12 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
     "Function": "A piece of code that you can easily call over and over again.",
-    "loop": "The action of doing something over and over again",
+    
 }
 
 #Retrieving an item from dictionary
-print(programming_dictionary["Bug"])
+#print(programming_dictionary["Bug"])
+
+#Adding an item to the dictionary
+programming_dictionary["loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
